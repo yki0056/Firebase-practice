5ddd9cd6c0c2affa495b04be11b83461aae92cf7
mattyun <yki0056@gmail.com>
2021-08-23 15:30:35 -0700

    initialize react project

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+    return (
+        <div>
+            ddd
+            이상한데
+
+        </div>
+    )
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+    entry: {
+        main: ['./src/main.js']
+    },
+    output: {
+        path: path.resolve(__dirname, './build'),
+        filename: '[name].js',
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            include: path.resolve(__dirname, './src'),
+            loader: 'babel-loader'
+        }]
+    },
+    plugins: [],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, './public'),
+        },
+        host: 'localhost',
+        port: 8080
+    }
+}
